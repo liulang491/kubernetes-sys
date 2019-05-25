180fe39dcafa5d70666bd1a7c8bb325fca571933
yingzhuo <yingzhor@gmail.com>
2019-05-25 14:24:51 +0800

    update

--- dashboard/show-token.sh
+++ dashboard/show-token.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-kubectl -n kube-system get secret | grep admin-toke | awk '{print $1}' | xargs kubectl -n kube-system describe secret
-
-exit 0
+kubectl -n kube-system get secret | grep admin-toke | awk '{print $1}' | xargs kubectl -n kube-system describe secret | grep token: | awk -F ' ' '{print $2}'
